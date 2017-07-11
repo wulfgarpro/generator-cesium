43b886158308809b111008c99524bdcedb273730
James Fraser <wulfgar.pro@gmail.com>
2017-07-11 13:36:55 +1000

    basic prompting for cesium root uri path added

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,4 +4,14 @@ module.exports = class extends Generator {
 	initializing() {
 		this.composeWith(require.resolve('generator-express/app'), {});
 	}
+	prompting() {
+		return this.prompt([{
+			type: 'input',
+			name: 'path',
+			message: 'Your Cesium URI path (e.g. /cesium)',
+			default: '/'
+		}]).then((answers) => {
+			this.log('URI path is', answers.path);
+		});
+	}
 };
