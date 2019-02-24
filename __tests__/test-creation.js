301d1ba75ec04c8129e8ef2ad687bc4f796b4584
James Fraser <wulfgar.pro@gmail.com>
2019-02-24 16:50:08 +1100

    Basic tests + .vsode

--- __tests__/test-creation.js
+++ __tests__/test-creation.js
@@ -1,37 +1,89 @@
 'use strict';
 
-var path = require('path');
-var helpers = require('yeoman-test');
-var assert = require('yeoman-assert');
-var rimraf = require('rimraf');
+const Generator = require('../generators/app');
+jest.mock('yeoman-generator');
 
 describe('generator-cesium/app Test', () => {
+  var cesiumGenerator;
   beforeEach(() => {
-     return helpers.run(path.join(__dirname, '../generators/app'))
-      .inDir(path.join(__dirname, 'tmp'))
-      .withPrompts({
-        viewerContext: '/',  // eslint-disable-line camelcase
-        appName: 'app'       // eslint-disable-line camelcase
-      })
+    cesiumGenerator = new Generator();
+    Object.defineProperty(cesiumGenerator, 'fs', { get: () => Object });
+    Object.defineProperty(cesiumGenerator, 'props', { get: () => Object });
+    cesiumGenerator.fs.copyTpl = jest.fn();
   });
+
   afterEach(() => {
-    rimraf.sync(path.join(__dirname, 'tmp'));
+    jest.resetAllMocks();
   });
 
-  it('creates expected custom files', function () {
-    assert.file([
-      'app/app.js',
-      'app/public/html/index.html',
-    ]);
-
-    assert.fileContent([
-      ['app/app.js', /cesium/],
-      ['app/public/html/index.html', /cesiumgateway/],
-    ]);
+  describe('generator functions', () => {
+    it('has initializing()', function () {
+      expect(cesiumGenerator.initializing).toBeDefined();
+    });
+    it('has prompting()', function () {
+      expect(cesiumGenerator.prompting).toBeDefined();
+    });
+    it('has writing()', function () {
+      expect(cesiumGenerator.writing).toBeDefined();
+    });
+    it('has conflicts()', function () {
+      expect(cesiumGenerator.conflicts).toBeDefined();
+    });
+    it('has install()', function () {
+      expect(cesiumGenerator.install).toBeDefined();
+    });
+    it('has end()', function () {
+      expect(cesiumGenerator.end).toBeDefined();
+    });
   });
 
+  describe('resetDestinationRoot()', () => {
+    it('calls Generator\'s destinationRoot()', function () {
+      cesiumGenerator.resetDestinationRoot('somepath');
+      expect(cesiumGenerator.destinationRoot).toHaveBeenCalledTimes(1);
+      expect(cesiumGenerator.destinationRoot).toHaveBeenCalledWith('somepath');
+    });
+  });
 
-  xit('creates expected express files', function () {
-    // TODO
+  describe('cesiumizeExpress()', () => {
+    it('calls Generator\'s fs.copyTpl() for app.js template', function () {
+      cesiumGenerator.props.app_name = 'someapp'; // eslint-disable-line camelcase
+      cesiumGenerator.props.viewer_context = 'somecontext'; // eslint-disable-line camelcase
+
+      cesiumGenerator.cesiumizeExpress();
+      expect(cesiumGenerator.fs.copyTpl).toHaveBeenCalledTimes(2);
+      expect(cesiumGenerator.templatePath).toHaveBeenCalledWith('app.js');
+      expect(cesiumGenerator.destinationPath).toHaveBeenCalledWith('someapp/app.js');
+      expect(cesiumGenerator.templatePath).toHaveBeenCalledWith('public/html');
+      expect(cesiumGenerator.destinationPath).toHaveBeenCalledWith('someapp/public/html');
+    });
+  });
+
+  describe('configuring()', () => {
+    it('calls Generator\'s destinationRoot()', function () {
+      cesiumGenerator.configuring();
+      expect(cesiumGenerator.destinationRoot).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('install()', () => {
+    it('calls Generator\'s npmInstall()', function () {
+      cesiumGenerator.install();
+      expect(cesiumGenerator.npmInstall).toHaveBeenCalledTimes(1);
+      expect(cesiumGenerator.npmInstall).toHaveBeenCalledWith(['cesium'], { save: true });
+    });
+  });
+
+  describe('end()', () => {
+    it('calls Generator\'s resetDestinationRoot() and then cesiumizeExpress()', function () {
+      jest.spyOn(cesiumGenerator, 'resetDestinationRoot');
+      jest.spyOn(cesiumGenerator, 'cesiumizeExpress');
+      cesiumGenerator.root = 'someroot';
+
+      cesiumGenerator.end();
+      expect(cesiumGenerator.resetDestinationRoot).toHaveBeenCalledTimes(1);
+      expect(cesiumGenerator.resetDestinationRoot).toHaveBeenCalledWith('someroot');
+      expect(cesiumGenerator.cesiumizeExpress).toHaveBeenCalledTimes(1);
+    });
   });
 });
--- __tests__/test-load.js
+++ __tests__/test-load.js
@@ -1,11 +1,7 @@
-/*global describe, it */
-
 'use strict';
 
-var assert  = require('assert');
-
 describe('generator-cesium', function () {
-    it('can be imported without blowing up', function () {
-        assert(require('../generators/app') !== undefined);
-    });
+  it('can be imported without blowing up', function () {
+    expect(require('../generators/app') !== undefined);
+  });
 });
